a2ee8aabd5f35ea258fa8e2677624bd770989198
AnhQuan9x99m <quannguyen1999qt@gmail.com>
2022-09-20 10:05:55 +0700

    push css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import {Layout} from 'antd' 
 import styles from '../styles/Home.module.css'
+import Sider from 'antd/lib/layout/Sider';
+import { Content, Footer } from 'antd/lib/layout/layout';
 
 export default function Home() {
   return (
-   console.log('ahihi')
+   <Layout>
+    <Sider>Sider</Sider>
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+   </Layout>
   )
 }
